2ae97ab516d44f4187de47968de60ce3ab81a496
CORP\andrey.korsakov <andrey.korsakov@solbeg.com>
2018-05-22 11:26:10 +0300

    Add project files.

---
+++ ConsoleDI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDI
+{
+    public class ConsoleMessageWriter : IMessageWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+    public interface IMessageWriter
+    {
+        void Write(string message);
+    }
+
+    public class Salutation
+    {
+        private readonly IMessageWriter writer;
+        public Salutation(IMessageWriter writer)
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+            this.writer = writer;
+        }
+        public void Exclaim()
+        {
+            this.writer.Write("Hello DI!");
+        }
+     }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IMessageWriter writer = new ConsoleMessageWriter();
+            var satul = new Salutation(writer);
+            satul.Exclaim();
+            Console.ReadLine();
+        }
+    }
+}
